12d22a13070aab742d101339629112b61b58ce84
HardeepNain <hnspnain@gmail.com>
2022-05-14 11:13:46 +0530

    updating notes

--- Module 1/Lec1_Basic/intro.js
+++ Module 1/Lec1_Basic/intro.js
@@ -5,27 +5,29 @@
 
 /*2.Data Types, Decalartion, Assign and Scope*/
 
-// Data Types =>
+/*
+2.1 Data Types =>
 
-// Java => Primitive => int , float , double , boolean
-// Non Primitive => Arrays , Stacks
+Java => Primitive => int , float , double , boolean
+        Non Primitive => Arrays , Stacks
 
-// Primitive
-// Javascript => Number(int , float , double) , Boolean , String(  "" , '' ) , undefined , Null , Object
+2.1.1 Javascript Primitive =>
+Number(int , float , double) , Boolean , String(  "" , '' ) , undefined , Null , Object
 
- // Datatype name = value;
- // int a = 10;
+Datatype name = value;
+int a = 10;
 
 
- // ES6 => Ecma Script 6
-// Two keyword let and const
+let and const
+ES6 => Ecma Script 6
 
+*/
+// Let :
 // Dynamic casting
 let a = 10;  // you have declared a variable with name a and initialized it with value 10;
-// let keyword => block scoped
-
 // console.log(a);
 
+// let keyword => block scoped
 if(true){
    let a = 20;
 //    console.log(a);
@@ -33,21 +35,23 @@ if(true){
 
 // console.log(a);
 
-
-// const => constant => block scoped and constant
-
-// declare and assign should be in one line in case of const in js
-// you can't declare in one and assign in another in case of const in javascript like this 
 /*
+Const : 
+constant => block scoped and constant
+
+declaration and assign should be in one line in case of const in js
+you can't declare in one and assign in another in case of const in javascript like this 
+
 const pi;
 pi = 3.14;
 
 this is wrong you can't do this in javascript.
  */
+
 const pi = 3.14;  // const variables remain constant throughout of the program they can't be changed.
 // console.log(pi);
 
-
+// in js you assign any type of value in let and const 
 let c = true;
 let d = 3.14;
 let e = 'Hey i am a string';
@@ -55,7 +59,7 @@ let f = undefined;
 let g;  // you can let any vairable not assigned anything in case of let which automatically by default get undefined.
 // console.log(g);
 
-// Non Primitive
+// 2.1.2 Non Primitive
 
 // Arrays=>
 // int[] a = new int[5];
@@ -66,20 +70,21 @@ let values = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 ];
 // add some data in values array
 values.push("Captain america !!");
 // console.log(values);
-// function , object , string , int , boolean 
+// function , object , string , int , boolean
 
 // console.log(values.pop());
 
 // console.log(values);
 
-//push => add at the end 
+//push => add at the end
 //pop => delete from the end
 
-// shift => delete a element from the starting 
+// shift => delete a element from the starting
 // unshift => add a element in the starting of the array
 // console.log(values.shift());
 // console.log(values);
 
+/*3 Objects */
 // Objects => key values pair : objects are the pair of keys and values.
 // keys => unique
 
